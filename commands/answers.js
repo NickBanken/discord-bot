94a8ef19e16e18c5970e324a117652c54cba2a68
Tanya leenders <tanya.leenders@gmail.com>
2021-05-31 10:48:24 +0200

    group answer management + made it wase-insensitive wip

--- commands/answers.js
+++ commands/answers.js
@@ -2,10 +2,19 @@ module.exports = {
     name: 'answers',
     description: 'looks for some words and answers accordingly',
     execute(message) {
+        lowerCaseMessage = message.shift().toLowerCase(); 
+
+        const helloArray = ["hello", "hi", "good morning"];
         const sadArray = ["problem"];
 
+        for (let i = 0; i < helloArray.length; i++) {
+            if (lowerCaseMessage.content.includes(helloArray[i])) {
+                message.channel.send(`Hello ${message.author}, how are you today?`);
+            }
+        }
+
         for (let i = 0; i < sadArray.length; i++) {
-            if (message.content.includes(sadArray[i])) {
+            if (lowerCaseMessage.content.includes(sadArray[i])) {
                 message.channel.send(`It's gonna be okayyy ${message.author}`);
             }
         }
--- index.js
+++ index.js
@@ -23,12 +23,12 @@ client.once('ready', () => {
 
 client.on('message', message => {
 
-    const helloArray = ["hello", "hi", "good morning"];
-    for (let i = 0; i < helloArray.length; i++) {
-        if (message.mentions.has(client.user) && (message.content.includes(helloArray[i]))) {
-            client.commands.get('hello').execute(message);
-        }
-    }
+    // const helloArray = ["hello", "hi", "good morning"];
+    // for (let i = 0; i < helloArray.length; i++) {
+    //     if (message.mentions.has(client.user) && (message.content.includes(helloArray[i]))) {
+    //         client.commands.get('hello').execute(message);
+    //     }
+    // }
 
     if (message.mentions.has(client.user)) {
         client.commands.get('answers').execute(message);
