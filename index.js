d004aafa09a89d8aed51db21667da147381f8744
Tanya-Amber-L <tanya.leenders@gmail.com>
2022-07-14 11:32:52 +0200

    added DS2 reminder messages

--- index.js
+++ index.js
@@ -64,7 +64,8 @@ client.on("message", (message) => {
 client.on("ready", () => {
 	const HamiltonChan = client.channels.cache.get("936210193191161876");
 	const JepsenChan = client.channels.cache.get("879651560462884896");
-	client.commands.get("reminder").execute(HamiltonChan, JepsenChan);
+	const DSChan = client.channels.cache.get("993813864162537482");
+	client.commands.get("reminder").execute(HamiltonChan, JepsenChan, DSChan);
 
 	client.user.setActivity("*help", { type: "WATCHING" }).catch(console.error);
 });
